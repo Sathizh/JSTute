61bd726b3e19ea61c5160c4e2b21582623e376ad
Sathish Muniyappan <sathish.muniyappan@boeing.com>
2024-01-25 10:18:00 +0530

    Day 2 Exercise added

--- Exercise/Day2Ex.js
+++ Exercise/Day2Ex.js
@@ -1,5 +1,30 @@
-/* ===================================================================
+/* ============================================================================================
 
     EX 2 : 
+            1. Declare variables for a person's information (name, age, and country)
+            2. Initialize them with appropriate values
 
-====================================================================*/
+            3. Create variables of different types: string, number, boolean, array, and object
+            
+            4. Declare a variable and assign an initial value
+            5. Reassign the variable with a new value
+
+            6. Create two variables containing strings and concatenate them
+
+            7. Use template literals to create a sentence using variables using backtick operator
+
+            8. Declare variables with values of null and undefined
+
+            9. Declare a variable globally and within a function block, observe the scope difference
+                
+                Hints:
+                    let globalVar = "I am global";
+                        
+                    {
+                        let localVar = "I am local";
+                        TODO: Access globalVar and localVar here
+                    }
+                    TODO: Try accessing localVar outside the function-Block - what happens?
+            
+
+===============================================================================================*/
