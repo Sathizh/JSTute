fd76645043f29c602d225115a5a03401462bc52c
Sathish Muniyappan <sathish.muniyappan@boeing.com>
2024-01-26 17:40:08 +0530

    Day 3 Typecasting Exercises

---
+++ Exercise/Day3Ex.js
@@ -0,0 +1,26 @@
+/* ============================================================================================
+
+    Day3 : 
+            Exercise 1: String to Number
+                Convert the string variables to numbers and perform addition
+
+            Exercise 2: Number to String
+                Convert the number variables to strings and concatenate them
+
+            Exercise 3: Boolean to Number
+                Convert the boolean variables to numbers and perform subtraction
+            
+            Exercise 4: Implicit Type Casting
+                Identify the result of the implicit type casting in the following expressions
+                
+                let implicitResult1 = "5" + 2;
+                let implicitResult2 = "5" - 2;
+                let implicitResult3 = "5" * 2;
+                let implicitResult4 = "5" / 2;
+
+                console.log(implicitResult1); // What is the result?
+                console.log(implicitResult2); // What is the result?
+                console.log(implicitResult3); // What is the result?
+                console.log(implicitResult4); // What is the result?            
+
+===============================================================================================*/
