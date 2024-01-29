92294c3c4c045547075371cb0b851faf3639479f
Sathish Muniyappan <sathish.muniyappan@boeing.com>
2024-01-29 09:16:35 +0530

    Day 5 Assignment

---
+++ Exercise/Day5Ex.js
@@ -0,0 +1,47 @@
+/* ============================================================================================
+
+    Day 5 : 
+            Exercise 1: Declare two boolean variables. Use logical AND, OR, and NOT operators 
+                to combine or negate them.
+
+            Exercise 2: Declare two variables and use comparison operators to compare them.
+                Store the results in boolean variables.
+
+            Exercise 3: Declare a variable representing a person's age. 
+                Use the ternary operator to determine if the person is eligible to vote.
+            
+            
+            Exercise 4: Declare variables of different types (number, string, boolean, etc.).
+                Use the typeof operator to display the data type of each variable.
+
+            
+            Exercise 5: Increment and Decrement Operations
+            
+                TODO: Observe the values
+                    let x = 8;
+                    let y = 3;
+
+                    x++;
+                    y--;
+
+                    console.log("After incrementing x:", x);
+                    console.log("After decrementing y:", y);
+
+                    let result = x++ + --y;
+
+                    console.log("Result of expression:", result);
+                    console.log("Final value of x:", x);
+                    console.log("Final value of y:", y);
+
+            
+            Exercise 6: Logical Operator
+                TODO: Observe the values
+                    let isSunny = true;
+                    let isWarm = false;
+
+                    console.log("Is it sunny and warm?", isSunny && isWarm);
+                    console.log("Is it either sunny or warm?", isSunny || isWarm);
+                    console.log("Is it not sunny?", !isSunny);
+
+
+===============================================================================================*/
