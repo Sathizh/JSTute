985f49ac997ffc2a975fef2c29f31f557cb06edb
Sathish Muniyappan <sathish.muniyappan@boeing.com>
2024-01-30 14:06:30 +0530

    Day 6 Exercise update

---
+++ Exercise/Day6Ex.js
@@ -0,0 +1,102 @@
+/* ============================================================================================
+
+    Day 6 : 
+
+        ! ▄︻̷̿┻̿═━一ᵂᵃʳⁿⁱⁿᵍ❣︎⚟ DON'T EXECUTE THE CODE, PREDICT THE OUTPUT
+        
+        
+                     ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! 
+                     !                                                                                           !
+                     !        ░░░░                                                                               !
+                     !                                                                                           !
+                     !                                                                                           !
+                     !                                            ██                                             !
+                     !                                          ██░░██                                           !
+                     !  ░░          ░░                        ██░░░░░░██                            ░░░░         !
+                     !                                      ██░░░░░░░░░░██                                       !
+                     !                                      ██░░░░░░░░░░██                                       !
+                     !                                    ██░░░░░░░░░░░░░░██                                     !
+                     !                                  ██░░░░░░██████░░░░░░██                                   !
+                     !                                  ██░░░░░░██████░░░░░░██                                   !
+                     !                                ██░░░░░░░░██████░░░░░░░░██                                 !
+                     !                                ██░░░░░░░░██████░░░░░░░░██                                 !
+                     !                              ██░░░░░░░░░░██████░░░░░░░░░░██                               !
+                     !                            ██░░░░░░░░░░░░██████░░░░░░░░░░░░██                             !
+                     !                            ██░░░░░░░░░░░░██████░░░░░░░░░░░░██                             !
+                     !                          ██░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░██                           !
+                     !                          ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██                           !
+                     !                        ██░░░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░░░██                         !
+                     !                        ██░░░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░░░██                         !
+                     !                      ██░░░░░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░░░░░██                       !
+                     !        ░░            ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██                       !
+                     !                        ██████████████████████████████████████████                         !
+                     !                                                                                           !
+                     ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! 
+    
+    
+            Initialize a variable
+
+            ?Exercise 1: Pre-increment
+                Increase the value of 'num' before using its current value
+
+            ?Exercise 2: Post-increment
+                Increase the value of 'num' after using its current value
+                TODO: Display the results
+
+            ?Exercise 3: Pre-increment with assignment
+
+                let a = 3;
+                let b = ++a + 2;
+
+                TODO: Predict the values of 'a' and 'b' after this operation
+            
+            ?Exercise 4: Post-increment with assignment
+
+                let x = 7;
+                let y = x++ * 2;
+
+                TODO: Predict the values of 'x' and 'y' after this operation
+
+            ?Exercise 5: Mixing pre-increment and post-increment
+
+                let m = 10;
+                let n = m++ + ++m;
+
+                TODO: Predict the values of 'm' and 'n' after this operation
+
+            ?Exercise 6: Predict the output
+
+                let x = 3;
+                let y = 7;
+
+                let result = (++x * 2) + (y-- * 3) + (++y + x++) + (--x * 2);
+
+                TODO: Predict the final value of 'result'
+
+            ?Exercise 7: 
+                let a = 5;
+                let b = 3;
+                let c = 8;
+
+                let resultA = (++a * 2) - (b++ * 3) + (c-- + ++b) * 2;
+                let resultB = (--a + ++b) * (c++ - a--);
+
+                TODO: Predict the final values of 'resultA' and 'resultB'
+            
+            ?Exercise 8: string Comparison
+
+                let str1 = "banana", str2 = "apple"
+                console.log("Result =", str1 <= str2);
+
+                let str3 = "pineapple", str4 = "Apple"
+                console.log("Result =", str3 >= str4);
+
+                let str5 = "APPLE", str6 = "apple"
+                console.log("Result =", str5 == str6);
+
+                let str7 = "goa", str8 = "sao"
+                console.log("Result =", str7 != str8);
+
+                TODO: Predict the outputs
+            
+            ===============================================================================================*/
